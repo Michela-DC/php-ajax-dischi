6d1b51e22efeac942f5c337b807f2a59d8bfffb8
Michela-DC <micheladelconte@outlook.com>
2022-03-31 20:19:11 +0200

    Added albums filtered by genre

--- Milestone 2/script.js
+++ Milestone 2/script.js
@@ -4,6 +4,10 @@ const app = new Vue ({
     el: '#app',
     data: {
         albums: [],
+        metal: [],
+        rock: [],
+        pop: [],
+        jazz: [],
     },
 
     methods: {
@@ -13,11 +17,36 @@ const app = new Vue ({
 
                 this.albums = res.data;
                 console.log(this.albums);
-            })
+            });
+        },
+
+        filteredAlbums () {
+            axios.get('./server.php').then (res => {
+
+                this.getGenre(res.data, 'metal',this.metal);
+                this.getGenre(res.data, 'rock',this.rock);
+                this.getGenre(res.data, 'pop',this.pop);
+                this.getGenre(res.data, 'jazz',this.jazz);
+                
+            }); 
+        },
+
+        getGenre (response, genre, array) { //funzione per filtrare la risposta dall'axios
+
+            for(let i = 0; i < response.length; i++){
+
+                if(response[i].genre.toLowerCase() === genre){
+
+                    array.push(response[i]);
+                }
+            }
+
+            console.log(array);
         }
     },
 
     mounted() {
         this.fetchData(); //invoco la funzione fetchData quando il file è tutto montato
+        this.filteredAlbums();
     }
 });
